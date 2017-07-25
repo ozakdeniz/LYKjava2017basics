c22347507d10675a914f4ce50df3053b804ba95c
Ozgur Akdeniz <tiny.weeny.xo@gmail.com>
2017-07-25 18:48:34 +0300

    initial commit

---
+++ src/tr/org/kamp/linux/firstApp/Hellojava.java
@@ -0,0 +1,8 @@
+package tr.org.kamp.linux.firstApp;
+
+public class Hellojava {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/tr/org/kamp/linux/veriTipleri/VeriTipleri.java
@@ -0,0 +1,11 @@
+package tr.org.kamp.linux.veriTipleri;
+
+public class VeriTipleri {
+	public int a = 65;
+	private int b = 45;
+	protected char c = 'A';
+	
+	int toplam = a+b;
+	
+
+}
